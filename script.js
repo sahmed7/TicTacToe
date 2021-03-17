ef4c170131bce9cb26ef26609aace2984a732f7a
Shaheer Ahmed <sahmed3721@gmail.com>
2021-03-17 12:42:37 -0500

    added initial win condition check functionality

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ let squares = ticTacGrid.querySelectorAll('.square')
 let playerTurn = true  //if true, Player 1 turn, else Player 2 turn
 let boardFilled = []
 
+let winConditions = {
+    topRow: ['X','O','X'],
+    middleRow: ['X','O','X'],
+    bottomRow: ['O','X','O'],
+    leftColumn: ['X','O','O'],
+    middleColumn: ['O','O','X'],
+    rightColumn: ['X','O','X'],
+    leftDiagonal: ['X','X','X'],
+    rightDiagonal: ['O','O','X'],
+}
+let playerOneCount = 0
+let playerTwoCount = 0
 console.log(ticTacGrid)
 console.log(squares)
 
@@ -12,6 +24,36 @@ const userSquareSelection = (function()  {  //had to use named functions to be a
     let placeUserSelection = function(event) { //as there wasn't a way to do it within the listener definition
         if (playerTurn) {
             event.target.innerText = 'X'
+            for (let winConditionsKey in winConditions) {
+                if(winConditions[winConditionsKey].length==3) {
+                    for(let i=0; i<3; i++) {
+                        if(winConditions[winConditionsKey][i] == 'X') {
+                            playerOneCount++
+                            console.log(`Player One Count: ${playerOneCount}`)
+                        } else {
+                            playerTwoCount++
+                            console.log(`Player One Count: ${playerTwoCount}`)
+                        }
+                        if(playerOneCount == 3) {
+                            console.log(`Winner in: ${winConditionsKey} , ${winConditions[winConditionsKey]}`, playerOneCount)
+                            return
+                        }
+                        if(playerTwoCount == 3) {
+                            console.log(`Winner in: ${winConditionsKey} , ${winConditions[winConditionsKey]}`, playerTwoCount)
+                            return
+                        }
+                    }
+                    console.log("No winner yet...")
+                    playerOneCount=0
+                    playerTwoCount=0
+
+                    // winConditionsKey.forEach((condition) => {
+                    //     if(condition)
+                    //         }
+                    //console.log(winConditions[winConditionsKey])
+                }
+
+            }
             //console.log(event.target.innerText)
             this.removeEventListener('click', placeUserSelection)
             playerTurn = false
