143c2ea38f0cbf0b219b4f17222df1730d77885d
Shaheer Ahmed <sahmed3721@gmail.com>
2021-03-17 11:26:06 -0500

    added game recognition of user selection in grid so that user can't select same square

--- script.js
+++ script.js
@@ -8,19 +8,47 @@ let boardFilled = []
 console.log(ticTacGrid)
 console.log(squares)
 
+const userSquareSelection = (function()  {  //had to use named functions to be able to remove EventListener
+    let placeUserSelection = function(event) { //as there wasn't a way to do it within the listener definition
+        if (playerTurn) {
+            event.target.innerText = 'X'
+            //console.log(event.target.innerText)
+            this.removeEventListener('click', placeUserSelection)
+            playerTurn = false
+        } else {
+            event.target.innerText = 'O'
+            //console.log(event.target.innerText)
+            this.removeEventListener('click', placeUserSelection)
+            playerTurn = true
+        }
+    };
+    return placeUserSelection;
+})( 0 );
+
 squares.forEach((sq) => {
-        sq.addEventListener(('click'), event => {
-            if (playerTurn) {
-                event.target.innerText = 'X'
-                playerTurn = false
-            } else {
-                event.target.innerText = 'O'
-                playerTurn = true
-            }
+        sq.addEventListener(('click'), userSquareSelection);
+            // if (playerTurn) {
+            //     event.target.innerText = 'X'
+            //     playerTurn = false
+            // } else {
+            //     event.target.innerText = 'O'
+            //     playerTurn = true
+            // }
             //console.log(event.target.innerText)
             //console.log('I\'ve been clicked!')
-        })
+        //})
 })
+
+
+
+
+
+// function acquiredStone(stoneReponse) {
+//     let index = gauntlet.indexOf(stoneReponse);
+//     if (index > -1) {
+//         gauntlet.splice(index, 1);
+//     }
+// }
 
 
 
